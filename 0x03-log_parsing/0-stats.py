980d07528a81e598b0fed3b41526ae8d39f027e3
stepholo <oloobrian89@gmail.com>
2024-02-11 08:14:24 -0800

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,37 @@
 #!/usr/bin/python3
+""""Log Parsing"""
+
 
 import sys
 
-
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-stats = {}
-allowed = [200, 301, 400, 401, 403, 404, 405, 500]
-count = 0
-
-
-def show_stats():
-    '''Print the statistics'''
-    print("File size: {}".format(total_size))
-
-    for stat in sorted(stats.keys()):
-        print("{}: {}".format(stat, stats.get(stat)))
-
+counter = 0
 
 try:
     for line in sys.stdin:
-        line = line.strip().split()
-        if (len(line) != 10 or line[5] != "GET" or line[6] != "/projects/260"
-           or line[8] != "HTTPS/1.1"):
-            continue
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-        status_code = int(line[7])
-        file_size = int(line[9])
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-        if status_code not in allowed:
-            continue
-
-        stats[status_code] = stats.get(status_code, 0) + 1
-        total_size += file_size
-
-        count += 1
-        if count == 10:
-            show_stats()
-            count = 0
-
-except KeyboardInterrupt:
+except Exception as err:
     pass
+
 finally:
-    show_stats()
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
